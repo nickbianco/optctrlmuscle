de0560807ae6863fd54b0d3725701c0e0cff5897
Christopher Dembia <cld72@cornell.edu>
2016-12-28 20:14:55 -0800

    Clean up gait1018 ankle clutched spring example.

--- Examples/gait10dof18muscles_ankle_clutched_spring/gait10dof18musc_ankle_clutched_spring.m
+++ Examples/gait10dof18muscles_ankle_clutched_spring/gait10dof18musc_ankle_clutched_spring.m
@@ -13,14 +13,14 @@ end
 Datapath = fullfile(getenv('OPENSIM_HOME'), 'Models', 'Gait10dof18musc', ...
     'OutputReference');
 IK_path=fullfile(Datapath,'IK','subject01_walk_IK.mot');
+%ID_path=fullfile(Datapath,'ID','inversedynamics.sto');
 ID_path=[]; % compute ID from the external loads
 model_path=fullfile(Datapath,'subject01.osim');
 time=[0.7 1.4];     % Part of the right stance phase
-OutPath=fullfile(MainDir,'Examples','OpenSimInstallation_Gait10dof18m','Results');
+OutPath=fullfile(DirExample,'Results');
 
 Misc.DofNames_Input={'ankle_angle_r','knee_angle_r','hip_flexion_r'};
 Misc.Loads_path=fullfile(Datapath,'ExperimentalData','subject01_walk_grf.xml');
-Misc.ID_ResultsPath=fullfile(Datapath,'ID','inversedynamics.sto');
 
 Misc.costfun = 'Exc_Act';
 Misc.ankle_clutched_spring = true;
