a884122a09ec06522940c9da23a354d4441c83e8
stingjp <stingjp@stanford.edu>
2019-07-10 11:41:01 -0700

    debugging a strange issue with gpops that seemed to eventually just fix itself.

--- Optimization/FtildeState/AnkleHipExosuit/Wrap4continous_Ftilde_vAAHE_Met.m
+++ Optimization/FtildeState/AnkleHipExosuit/Wrap4continous_Ftilde_vAAHE_Met.m
@@ -1,6 +1,8 @@
 function phaseout = Wrap4continous_Ftilde_vAAHE_Met(input)
 
+% input is good first time here
 persistent splinestruct
+% then splinestruct is not fine
 
 if isempty(splinestruct)|| size(splinestruct.MA,1) ~= length(input.phase.time) 
     splinestruct = SplineInputData(input.phase.time,input);
